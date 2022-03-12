ee060a306cbdd4e26aa495518efd5e49539fe56d
DragonikongX <NoNameFB@wp.pl>
2022-03-12 20:14:43 +0100

    Cos tam poszło

--- GreenScreen/GreenScreen.cpp
+++ GreenScreen/GreenScreen.cpp
@@ -29,8 +29,12 @@ static Mat img;
 // :: USTAWIENIA KOLORÓW ::
 
 Vec3f colorToDelete = Vec3f(182, 203, 203);
-float colorThreshold = 0.25; // 0 to 1
+float colorThreshold = 0.1; // 0 to 1
 Vec3f l_colorToDelete, h_colorToDelete;
+Vec2f mousePosition;
+int shader = 0;
+ShaderUtil shaderUtil;
+unsigned char* buff = new unsigned char[1228800];
 
 const int texNum = 2;
 GLuint textures[texNum];
@@ -43,6 +47,58 @@ void calculateColor() {
 	l_colorToDelete -= rangeVector;	
 	h_colorToDelete = colorToDelete/255;
 	h_colorToDelete += rangeVector;
+}
+
+void GreenScreenON() {
+	shaderUtil.Load("greenscreen.frag");
+	shaderUtil.Use();
+	glUniform1i(glGetUniformLocation(shaderUtil.mProgramId, "tex"), 0);
+	glUniform1i(glGetUniformLocation(shaderUtil.mProgramId, "img"), 1);
+	glUniform3f(glGetUniformLocation(shaderUtil.mProgramId, "low"), l_colorToDelete[0], l_colorToDelete[1], l_colorToDelete[2]);
+	glUniform3f(glGetUniformLocation(shaderUtil.mProgramId, "high"), h_colorToDelete[0], h_colorToDelete[1], h_colorToDelete[2]);
+}
+
+void usrednij(unsigned char* buff) {
+	int R = 0, G = 0, B = 0;
+	for (int i = 0; i < 27; i += 3) {
+		R += static_cast<int>(buff[i]);
+		G += static_cast<int>(buff[i + 1]);
+		B += static_cast<int>(buff[i + 2]);
+	}
+	R /= 9;
+	G /= 9;
+	B /= 9;
+	colorToDelete = Vec3f(R, G, B);
+	calculateColor();
+	glUniform3f(glGetUniformLocation(shaderUtil.mProgramId, "low"), l_colorToDelete[0], l_colorToDelete[1], l_colorToDelete[2]);
+	glUniform3f(glGetUniformLocation(shaderUtil.mProgramId, "high"), h_colorToDelete[0], h_colorToDelete[1], h_colorToDelete[2]);
+}
+
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
+{
+	if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+	{
+		double xpos, ypos;
+		glfwGetCursorPos(window, &xpos, &ypos);
+		mousePosition = Vec2f(xpos, ypos);
+		usrednij(buff);
+	}
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (key == GLFW_KEY_E && action == GLFW_PRESS) {
+		if (shader == 0) {
+			GreenScreenON();
+			shader = 1;
+			glUniform1i(glGetUniformLocation(shaderUtil.mProgramId, "shader"), shader);
+		}
+		else {
+			shader = 0;
+			glUniform1i(glGetUniformLocation(shaderUtil.mProgramId, "shader"), shader);
+
+		}
+	}
 }
 
 void BindCVMat2GLTexture(cv::Mat& image, GLuint& imageTexture)
@@ -77,8 +133,6 @@ void BindCVMat2GLTexture(cv::Mat& image, GLuint& imageTexture)
 		glGenerateMipmap(GL_TEXTURE_2D);
 	}
 }
-
-
 
 int main() {
 	GLFWwindow* window;
@@ -115,7 +169,6 @@ int main() {
 			}
 		}
 
-
 		fprintf(stdout, "Status: Using GLEW %s\n", glewGetString(GLEW_VERSION));
 		
 		calculateColor();
@@ -126,19 +179,17 @@ int main() {
 		flip(img, img, 0);
 
 		kamerka >> frame;
-		flip(frame, frame, 0);
 
 		// :: TRYB ORTOGRAFICZNY :: 
 
 		glMatrixMode(GL_PROJECTION);
-		glOrtho(0, 640, 0, 480, -1, 1);
-		glMatrixMode(GL_MODELVIEW);
+		glOrtho(0, 640, 0, 480, 1, -1);
+		
 
 		// :: BINDOWANIE TEKSTUR ::
 
-		GLuint textures[texNum];
 		glGenTextures(texNum, textures);
-
+		
 		glActiveTexture(GL_TEXTURE0);
 		BindCVMat2GLTexture(frame, textures[0]);
 		glActiveTexture(0);
@@ -150,17 +201,10 @@ int main() {
 		BindCVMat2GLTexture(img, textures[1]);
 		glActiveTexture(0);
 		glBindTexture(GL_TEXTURE_2D, 0);
-
-
-		// :: SHADER ::
-
-		ShaderUtil shaderUtil;
-		shaderUtil.Load("greenscreen.frag");
-		shaderUtil.Use();
-		glUniform1i(glGetUniformLocation(shaderUtil.mProgramId, "tex"), 0);
-		glUniform1i(glGetUniformLocation(shaderUtil.mProgramId, "img"), 1);
-		glUniform3f(glGetUniformLocation(shaderUtil.mProgramId, "low"), l_colorToDelete[0], l_colorToDelete[1], l_colorToDelete[2]);
-		glUniform3f(glGetUniformLocation(shaderUtil.mProgramId, "high"), h_colorToDelete[0], h_colorToDelete[1], h_colorToDelete[2]);
+		
+		
+		glfwSetKeyCallback(window, key_callback);
+		glfwSetMouseButtonCallback(window, mouse_button_callback);
 		
 		while (!glfwWindowShouldClose(window))
 		{
@@ -175,14 +219,15 @@ int main() {
 			flip(frame, frame, 0);
 
 			// :: PRZYPISANIE NOWYCH DANYCH DO ODPOWIEDNICH JEDNOSTEK TEKSTURUJĄCYCH
-
 			glActiveTexture(GL_TEXTURE0);
 			BindCVMat2GLTexture(frame, textures[0]);
-			glActiveTexture(GL_TEXTURE1); 
-			glBindTexture(GL_TEXTURE_2D, textures[1]);
-
+			if (shader == 1) {
+				glActiveTexture(GL_TEXTURE1);
+				glBindTexture(GL_TEXTURE_2D, textures[1]);
+			}
+			
 			// :: RENDER ::
-
+			
 			glEnable(GL_TEXTURE_2D);
 			glBegin(GL_QUADS);
 			glTexCoord2i(0, 0); glVertex2i(0, 0);
@@ -191,19 +236,23 @@ int main() {
 			glTexCoord2i(1, 0); glVertex2i(sizee.width, 0);
 			glEnd();
 			glDisable(GL_TEXTURE_2D);
-
+			
+			
+			
 			glfwSwapBuffers(window);
-
+			glReadPixels(mousePosition[0], 480 - mousePosition[1], 3, 3, GL_RGB, GL_UNSIGNED_BYTE, buff);
+			
+			//cout << mousePosition[0] << " " << mousePosition[1] << " k " << static_cast<int>(buff[0]) << " " << static_cast<int>(buff[1]) << " " << static_cast<int>(buff[2]) << endl;
+			//cout << mousePosition[0] << " " << mousePosition[1] << " k " << cos[0] << " " << cos[1] << " " << cos[2] << endl;
 			glfwPollEvents();
 		}
-
 		shaderUtil.Delete();
 		kamerka.release();
 		destroyAllWindows();
-
 	}
 
 	glfwTerminate();
 	return 0;
 }
+
 
